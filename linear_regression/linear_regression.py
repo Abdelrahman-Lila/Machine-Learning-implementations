ff7287270ad133d96f8811cb51228ec241e8c8fc
Abdelrahman Lila <abdelrahman.lila@ejust.edu.eg>
2025-11-26 11:47:03 +0200

    implemented linear regression model

---
+++ linear_regression/linear_regression.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+
+class LinearRegression:
+    def __init__(self, lr=0.001, num_iterations=10):
+        self.lr = lr
+        self.num_iterations = num_iterations
+        self.weights = None
+        self.bias = None
+
+    def fit(self, X, y):
+        num_samples, num_features = X.shape
+        self.weights = np.zeros(num_features)
+        self.bias = 0
+
+        for iteration in self.num_iterations:
+            y_pred = np.dot(X, self.weights) + self.bias
+
+            dw = (1 / num_samples) * np.dot(X.T, (y_pred - y))
+            db = (1 / num_samples) * np.sum(y_pred - y)
+
+            self.weights = self.weights - self.lr * dw
+            self.bias = self.bias - self.lr * db
+
+    def predict(self, X):
+        y_pred = np.dot(X, self.weights) + self.bias
+        return y_pred
